9380c33ebccb93f5eb4d07e94f8be1c206d9ce12
Stuart Taylor <stuart_taylor@rapid7.com>
2026-03-28 22:12:02 +0000

    Force delete ecr repo on teardown

--- ecr.tf
+++ ecr.tf
@@ -1,7 +1,8 @@
 resource "aws_ecr_repository" "tasky" {
   name                 = "tasky"
   image_tag_mutability = "MUTABLE"
-
+  force_delete         = true
+   
   image_scanning_configuration {
     scan_on_push = false
   }
