9a866fd6aacd646a497037666a08e84b4e48b026
Stuart Taylor <stuart_taylor@rapid7.com>
2026-03-28 11:02:39 +0000

    Added EKS security groups

--- security_groups.tf
+++ security_groups.tf
@@ -32,3 +32,57 @@ resource "aws_security_group" "mongodb" {
     Name = "${var.name_prefix}mongodb-sg"
   }
 }
+
+# Security group for EKS 
+resource "aws_security_group" "eks_cluster" {
+  name        = "${var.name_prefix}eks-cluster-sg"
+  description = "EKS control plane security group"
+  vpc_id      = aws_vpc.main.id
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name_prefix}eks-cluster-sg"
+  }
+}
+
+# Security group for EKS worker nodes
+resource "aws_security_group" "eks_nodes" {
+  name        = "${var.name_prefix}eks-nodes-sg"
+  description = "EKS worker node security group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "Control plane to node communication"
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "tcp"
+    security_groups = [aws_security_group.eks_cluster.id]
+  }
+
+  ingress {
+    description = "Node to node communication"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name_prefix}eks-nodes-sg"
+  }
+}
